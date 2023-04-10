14ccfa0f668627f213256152b08d290c599dd9fb
Bosch <boschio91@gmail.com>
2023-04-10 17:29:34 -0400

    Added basic keyboard input for movement.

--- src/com/game/main/Game.java
+++ src/com/game/main/Game.java
@@ -20,6 +20,7 @@ public class Game extends Canvas implements Runnable {
 	
 	public Game() {
 		handler = new Handler();
+		this.addKeyListener(new KeyInput(handler));
 		
 		new Window(WIDTH, HEIGHT, "Wave Dodger", this);
 		
--- src/com/game/main/Player.java
+++ src/com/game/main/Player.java
@@ -16,7 +16,8 @@ public class Player extends GameObject {
 	}
 	
 	public void render(Graphics g) {
-		g.setColor(Color.red);
+		if (id == ID.Player) g.setColor(Color.red);
+		else if (id == ID.Player2) g.setColor(Color.blue);
 		g.fillRect(x,y,32,32);
 	}
 
