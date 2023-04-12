d220dfc02a17916504ed1d0417ada9dfed59b69d
Boschio <boschio91@gmail.com>
2023-04-12 12:21:32 -0400

    Corrected movement bug for player when keys pressed simultaneously/too fast.

--- src/com/game/main/KeyInput.java
+++ src/com/game/main/KeyInput.java
@@ -6,6 +6,7 @@ import java.awt.event.KeyEvent;
 public class KeyInput extends KeyAdapter {
 	
 	private Handler handler;
+	private boolean[] keyDown = {false, false, false, false};
 	
 	public KeyInput(Handler handler) {
 		this.handler = handler;
@@ -20,15 +21,15 @@ public class KeyInput extends KeyAdapter {
 			if(tempObject.getId() == ID.Player) {
 				// key events for player 1
 				
-				if (key == KeyEvent.VK_W) tempObject.setVelY(-10);
-				if (key == KeyEvent.VK_A) tempObject.setVelX(-10);
-				if (key == KeyEvent.VK_S) tempObject.setVelY(10);
-				if (key == KeyEvent.VK_D) tempObject.setVelX(10);
+				if (key == KeyEvent.VK_W) { tempObject.setVelY(-10); keyDown[0] = true; }
+				if (key == KeyEvent.VK_A) { tempObject.setVelX(-10); keyDown[1] = true; }
+				if (key == KeyEvent.VK_S) { tempObject.setVelY(10); keyDown[2] = true; }
+				if (key == KeyEvent.VK_D) { tempObject.setVelX(10); keyDown[3] = true; }
 			}
 			
 			if(tempObject.getId() == ID.Player2) {
 				// key events for player 2
-				
+				// TODO Refactor to correct movement bug
 				if (key == KeyEvent.VK_UP) tempObject.setVelY(-10);
 				if (key == KeyEvent.VK_LEFT) tempObject.setVelY(-10);
 				if (key == KeyEvent.VK_DOWN) tempObject.setVelY(10);
@@ -48,15 +49,21 @@ public class KeyInput extends KeyAdapter {
 			if(tempObject.getId() == ID.Player) {
 				// key events for player 1
 				
-				if (key == KeyEvent.VK_W) tempObject.setVelY(0);
-				if (key == KeyEvent.VK_A) tempObject.setVelX(0);
-				if (key == KeyEvent.VK_S) tempObject.setVelY(0);
-				if (key == KeyEvent.VK_D) tempObject.setVelX(0);
+				if (key == KeyEvent.VK_W) keyDown[0] = false;
+				if (key == KeyEvent.VK_A) keyDown[1] = false;
+				if (key == KeyEvent.VK_S) keyDown[2] = false;
+				if (key == KeyEvent.VK_D) keyDown[3] = false;
+				
+				// Vertical Movement
+				if (!keyDown[0] && !keyDown[2]) tempObject.setVelY(0);
+				
+				// Horizontal Movement
+				if (!keyDown[1] && !keyDown[3]) tempObject.setVelX(0);
 			}
 			
 			if(tempObject.getId() == ID.Player2) {
 				// key events for player 2
-				
+				// TODO Refactor to correct movement bug
 				if (key == KeyEvent.VK_UP) tempObject.setVelY(0);
 				if (key == KeyEvent.VK_LEFT) tempObject.setVelX(0);
 				if (key == KeyEvent.VK_DOWN) tempObject.setVelY(0);
